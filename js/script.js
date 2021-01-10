77368e929fae1fdf1225b59d0b0c5dbc0284af65
WillianFarias <williansousafarias@gmail.com>
2021-01-10 16:03:56 -0300

    criando a cobrinha na tela

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 //cada retangulo tem 32px
 let box = 32;
+let snake = [];
+
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -11,4 +17,12 @@ function criarBG() {
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+function criarCobrinha() {
+  for (let i = 0; i < snake.length; i++) {
+    context.fillStyle = "green";
+    context.fillRect(snake[i].x, snake[i].y, box, box);
+  }
+}
+
 criarBG();
+criarCobrinha();
