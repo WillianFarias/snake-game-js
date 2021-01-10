b2b130c5dbdea7077efd99af8d6ea48dcb13698c
WillianFarias <williansousafarias@gmail.com>
2021-01-10 18:11:12 -0300

    projeto finalizado

--- js/script.js
+++ js/script.js
@@ -20,20 +20,21 @@ let food = {
 }
 
 function criarBG() {
-  context.fillStyle = "lightgreen";
+  context.fillStyle = "#A77048";
+  
   //desenhando retangolo
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
   for (let i = 0; i < snake.length; i++) {
-    context.fillStyle = "green";
+    context.fillStyle = "#274915";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
 function drawFood() {
-  context.fillStyle = "red";
+  context.fillStyle = "#E80404";
   context.fillRect(food.x, food.y, box, box);
 }
 
@@ -52,7 +53,7 @@ function iniciarJogo() {
   if (snake[0].y > 15 * box && direction === "down") snake[0].y = 0;
   if (snake[0].y < 0 && direction === "up") snake[0].y = 15 * box;
 
-  for (let i = 0; i < snake.length; i++) {
+  for (let i = 1; i < snake.length; i++) {
     if (snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
       clearInterval(jogo);
       alert('Game Over');
